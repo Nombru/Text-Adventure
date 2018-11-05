f03f4514708cac4a87a4caf48b07c424809e31c2
Brittany Gollins <brittany.gollins@gmail.com>
2018-11-04 18:36:13 -0800

    get Kyle form input to work

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,22 @@
+// biz logic
+var kyleInput;
+
+function kyleCorrect(){
+  $("#magicalKyle").hide();
+  $("#gate").show();
+}
+
+function kyleIncorrect(){
+  $("#magicalKyle").hide();
+  $("#dead2").show();
+}
+
+// user logic
 $(document).ready(function() {
+  $("#wakeUp, #looseRocks, #magicalKyle, #death1, #death2, #gate").hide();
+
 // Move from start page to first part of adventure
+
   $("#startButton").click(function() {
     console.log("start button on")
     $("#start").hide();
@@ -9,13 +26,13 @@ $(document).ready(function() {
 // Click on trapdoor button and died
   $("#trapDoor").click(function() {
     console.log("trap door button")
-    $("#wakeUp").hide();
+    $("#wakeUp, #looseRocks, #magicalKyle, #death2, #gate").hide();
     $("#death1").show();
   });
 // Click on button for ornate door
   $("#kyleDoor").click(function() {
     console.log("ornate door button to Kyle")
-    $("#wakeUp, #death1").hide();
+    $("#wakeUp, #death1, #looseRocks").hide();
     $("#magicalKyle").show();
   });
 
@@ -24,5 +41,20 @@ $(document).ready(function() {
     console.log("rubble door")
     $("#wakeUp, #death1, #magicalKyle").hide();
     $("#looseRocks").show();
+  });
+
+//Submit Answer to Kyle's Riddle
+$(".kyleForm").submit(function(event) {
+    event.preventDefault();
+
+    kyleInput = $("input#day").val();
+    console.log(kyleInput);
+
+    if (kyleInput = "wednesday"){
+      return kyleCorrect();
+    } else {
+      return kyleIncorrect();
+    }
+
   });
 });
