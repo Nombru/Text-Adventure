b17169a1d16934347510bbfe458c994ac699e7eb
Brittany Gollins <brittany.gollins@gmail.com>
2018-11-04 17:21:04 -0800

    add gif to trap door, add show and hide capability in js for trap door section

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,15 @@
 $(document).ready(function() {
+// Move from start page to first part of adventure
   $("#startButton").click(function() {
     console.log("start button on")
     $("#start").hide();
     $("#wakeUp").show();
+  });
+
+// Click on trapdoor button and died
+  $("#trapDoor").click(function() {
+    console.log("trap door button")
+    $("#wakeUp").hide();
+    $("#death1").show();
   });
 });
